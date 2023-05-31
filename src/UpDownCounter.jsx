7ee13bbdbf8138331eedcd86551147629a2e2f74
CarismaDAqueen <130988079+ccousins694@users.noreply.github.com>
2023-05-31 18:44:53 -0400

    reactquiz2retake

--- src/UpDownCounter.jsx
+++ src/UpDownCounter.jsx
@@ -1,13 +1,23 @@
 import React, {useState} from "react";
+import React from "react";
 
-export const UpDownCounter = () => {
+export const UpDownCounter = (props) => {
+    let [counter, setCounter] = useState(props.value)
 
+    const onHandleClick = (directon) => {
+        if (directon === "up") {
+            setCounter(++counter)
+        } else {
+            setCounter(--counter)
+        }
+    }
 
     return (
-        <>
-            <button>Down</button>
-            {}
-            <button>Up</button>
-        </>
+        <div className ="Main">
+            <button onClick={() => onHandleClick("down")}>Down</button>
+            {counter}
+            <button onClick={() => onHandleClick("up")}>Up</button>
+        </div>
+           
     )
 }
