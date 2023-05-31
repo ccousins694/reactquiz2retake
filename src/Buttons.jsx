366e726f89a7ebeb91d502dee4438e3a3cf09d69
CarismaDAqueen <130988079+ccousins694@users.noreply.github.com>
2023-05-31 18:56:15 -0400

    push

--- src/Buttons.jsx
+++ src/Buttons.jsx
@@ -1,12 +1,36 @@
 import React, {useState} from "react";
+import React from "react";
 
 export const Buttons = () => {
+    let [buttonText1, setButtonText1] = useState("Push");
+    let [buttonText2, setButtonText2] = useState("Push");
+
+    const onHandleClick = (number) => {
+        switch(number) {
+            case 1:
+                setButtonText1("Push");
+                break;
+    
+            case 2:
+                setButtonText2("Push");
+                break;
+            case 3:
+                setButtonText1("Push");
+                setButtonText2("Push")
+                break;
+                default;
+                break;
+
+    
+    }
+
 
     return (
         <>
-            <button>Push</button>
-            <button>Push</button>
-            <button>Reset</button>
+        <button onClick={() => onHandleClick("down")}>Down</button>
+>Push</button>
+        <button onClick={() => onHandleClick("down")}>Down</button>
+        <button>Reset</button>
         </>
     )
 }
